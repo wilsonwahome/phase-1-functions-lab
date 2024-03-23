f4574ebf2506baa52f8a98d71ecafeb62f7a3bac
wahome <wahomewgitonga@gmail.com>
2024-03-23 15:36:40 +0300

    update index.js

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(location) {
+    const hq = 42;
+    return Math.abs(location - hq);
+  }
+  
+  function distanceFromHqInFeet(location) {
+    const blocks = distanceFromHqInBlocks(location);
+    const feetPerBlock = 264;
+    return blocks * feetPerBlock;
+  }
+  
+  function distanceTravelledInFeet(start, destination) {
+    const blocks = Math.abs(destination - start);
+    const feetPerBlock = 264;
+    return blocks * feetPerBlock;
+  }
+  
+  function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+  
+    if (distance <= 400) {
+      return 0;
+    } else if (distance > 400 && distance <= 2000) {
+      return (distance - 400) * 0.02;
+    } else if (distance > 2000 && distance <= 2500) {
+      return 25;
+    } else {
+      return 'cannot travel that far';
+    }
+  }
+  
